316c963a31c1ed2bffb0c7ed00530bd2edf72bc9
NCanggoro <figuranajah@gmail.com>
2018-04-20 20:32:28 +0700

    add controllers

--- LibraryManagement/Controllers/CatalogController.cs
+++ LibraryManagement/Controllers/CatalogController.cs
@@ -36,5 +36,26 @@ namespace LibraryManagement.Controllers
 
             return View(model);
         }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = _asset.GetById(id);
+
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _asset.GetAuthorOrDirector(id),
+                CurrentLocation = _asset.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _asset.GetDeweyIndex(id),
+                ISBN = _asset.GetIsbn(id)
+            };
+
+            return View(model);
+        }
     }
 }
