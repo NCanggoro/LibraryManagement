3a45ea07ddaf980096d3d929b831a17b53cfc123
NCanggoro <figuranajah@gmail.com>
2018-05-01 16:29:54 +0700

    initial comit

---
+++ LibraryManagement/Controllers/BranchController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryData;
+using LibraryData.Models;
+using LibraryManagement.Models.Branch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class BranchController : Controller
+    {
+        private ILibraryBranch _branch;
+        public BranchController(ILibraryBranch branch)
+        {
+            _branch = branch;
+        }
+
+        public IActionResult Index()
+        {
+            var branches = _branch.GetAll().Select(b => new BranchDetailModel
+            {
+                Id = b.Id,
+                Name = b.Name,
+                IsOpen = _branch.IsBranchOpen(b.Id),
+                //if we have alot asset and patron we should seperate query for more efficent
+                NumberOfAssets = _branch.GetAssets(b.Id).Count(),
+                NumberOfPatrons = _branch.GetPatrons(b.Id).Count()
+            });
+
+            var model = new BranchIndexModel()
+            {
+                Branches = branches
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var branch = _branch.Get(id);
+
+            var model = new BranchDetailModel
+            {
+                Id = branch.Id,
+                Name = branch.Name,
+                Address = branch.Address,
+                Telephone = branch.Telephone,
+                OpenDate = branch.OpenDate.ToString("dd-MM-yyyy"),
+                NumberOfAssets = _branch.GetAssets(id).Count(),
+                NumberOfPatrons = _branch.GetPatrons(id).Count(),
+                TotalAssetValue = _branch.GetAssets(id).Sum(a => a.Cost),
+                ImageUrl = branch.ImageUrl,
+                HoursOpen = _branch.GetBranchHours(id)
+            };
+
+            return View(model);
+        }
+    }
+}
